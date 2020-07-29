60e7a2fe8df5aebe6ced1929d492f767f003efa4
Mishiko <nnamper@gmail.com>
2020-07-29 13:09:35 +0400

    Update googlesearchconsole.py
    PEP-8

--- googlesearchconsole.py
+++ googlesearchconsole.py
@@ -28,50 +28,36 @@ REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 
 
 def get_gsc_data(start,end):
-    start_date = datetime(int(start[0:4]),int(start[5:7]),int(start[8:10]))
-    end_date =  datetime(int(end[0:4]),int(end[5:7]),int(end[8:10]))
-    print(start_date,end_date)
+    start_date = datetime.strftime(start, "%Y-%m-%d")
+    end_date =  datetime.strftime(end, "%Y-%m-%d")
     maxRows = 25000
-    i = 0
     output_rows = []
-    date_2 = start_date - timedelta(days=1)
-    while  date_2 < end_date :
-        date_2 = date_2 + timedelta(days=1)    
-        date = date_2.strftime("%Y-%m-%d")
+    date = start_date - timedelta(days=1)
+    while  date < end_date :  
+        date = (date + timedelta(days=1)).strftime("%Y-%m-%d")
         i = 0 
         output_rows.clear()
-        while True:
-
-            request = {
+        request = {
                 'startDate' : date,
                 'endDate' : date,
                 'dimensions' : ["query","page","country","device"],
                 "searchType": "Web",
                 'rowLimit' : maxRows,
-                'startRow' : i * maxRows
-
             }
-        
+        while True:
+            request['startRow'] = i * maxRows
             response = webmasters_service.searchanalytics().query(siteUrl = SITE_URL, body=request).execute()
-            if response is None:
-                #print("there is no response")
-                break
-            elif 'rows' not in response:
-                #print("row not in response")
+            if response is None or 'rows' not in respone:
                 break
             else:
-                for row in response['rows']:
-                    keyword = row['keys'][0]  #query
-                    page = row['keys'][1]      
-                    country = row['keys'][2]   
-                    device = row['keys'][3]     
-                    output_row = [date, keyword, page, country, device, row['clicks'], row['impressions'], row['ctr'], row['position']]
-                    #print(output_row)
+                for row in response['rows']:  
+                    output_row = [date, *(row['keys'][0:4]), row['clicks'], row['impressions'], row['ctr'], row['position']]
                     output_rows.append(output_row)
-                i = i + 1
+                    
+                i += 1
                 df = pd.DataFrame(output_rows, columns=['date','query','page', 'country', 'device', 'clicks', 'impressions', 'ctr', 'avg_position'])
                 file_name=str(date)+"_gsc_output.csv"
                 df.to_csv("data_output/"+file_name)
 
 
-get_gsc_data('2020-01-10','2020-01-20') #yyyy-mm-dd
+get_gsc_data('2020-01-10','2020-01-20') # yyyy-mm-dd
